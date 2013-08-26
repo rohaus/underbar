2dbbf092706e2f6c04523226459f00b4bc81752b
Ryan Roxas <ryanaroxas@gmail.com>
2013-08-26 01:03:05 -0700

    Added 'invoke' function.

--- src/underbar.js
+++ src/underbar.js
@@ -137,6 +137,13 @@ var _ = { };
 
   // Calls the method named by methodName on each value in the list.
   _.invoke = function(list, methodName, args) {
+    return _.map(list, function(value, key, collection){
+      if (typeof(methodName) === 'string'){
+        return value[methodName]()
+      } else {
+        return methodName.apply(value)
+      }
+    })
   };
 
   // Reduces an array or object to a single value by repetitively calling
