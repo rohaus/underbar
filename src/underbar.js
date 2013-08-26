17f21a0b70666e9357b0c6f583bef4ebf790d298
Ryan Roxas <ryanaroxas@gmail.com>
2013-08-25 19:35:50 -0700

    Added 'filter' function.

--- src/underbar.js
+++ src/underbar.js
@@ -72,6 +72,13 @@ var _ = { };
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, iterator) {
+    var result = [];
+    _.each(collection,function(value){
+      if (iterator(value) === true){
+        result.push(value);
+      }
+    })
+    return result
   };
 
   // Return all elements of an array that don't pass a truth test.
